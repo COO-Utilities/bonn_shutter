b19013e839293ea01abc839d0ad1271a2d523edf
Elijah.AB <elijahab@caltech.edu>
2026-03-09 15:11:33 -0700

    Status function fix

--- python/bonn_shutter.py
+++ python/bonn_shutter.py
@@ -220,7 +220,7 @@ class BonnShutterController(HardwareMotionBase): #pylint: disable=R0902
         '''Get the status of the Bonn Shutter.'''
         if self.state['is_connected'] is False:
             raise RuntimeError("Shutter is not Connected")
-        
+
         if self._send_command(self.Cmds.CHECK_STATUS + " 1") is not True:
             raise RuntimeError("Failed too communicate with shutter")
         self.status["A"] = self._parse_sv(self._read_reply())
